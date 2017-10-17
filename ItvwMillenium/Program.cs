75d7c59803399b6e173971aa4bbb2c6d8e0258d5
cervelle <cervelle@gmail.com>
2017-10-17 08:36:14 -0400

    commit All

---
+++ ItvwMillenium/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ItvwMillenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = @"test_data.csv";
+            var Positions = new List<Position>();
+            var Net_Positions = new List<Position>();
+
+            using (var sr = new StreamReader(filePath))
+            {
+                string headerLine = sr.ReadLine();
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Positions.Add(new Position(line));  
+                }
+            }
+
+            
+        }
+    }
+
+    class Position
+    {
+        string Trader;
+        string Broker;
+        string Symbol;
+        int Quantity;
+        int Price;
+
+        public Position(string line)
+        {
+            string[] fields = line.Split(',');
+            Trader = fields[0];
+            Broker = fields[1];
+            Symbol = fields[2];
+            Quantity = int.Parse(fields[3]);
+            Price = int.Parse(fields[4]);
+        }
+    }
+}
