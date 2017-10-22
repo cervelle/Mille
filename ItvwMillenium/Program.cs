a070b4a41699a09aa0d827c80d1bad97419d5961
cervelle <cervelle@gmail.com>
2017-10-22 13:21:27 -0400

    Rework architecture + BoxedPositionsCalculator

--- ItvwMillenium/Program.cs
+++ ItvwMillenium/Program.cs
@@ -15,32 +15,53 @@ namespace ItvwMillenium
            
         }
 
-        public static void NetPositionsCalculator(string filePath)
+        public static void PositionsCalculator(string input, string type)
         {
-            var NetPositions = new SortedList();
+            var Positions = new SortedList();
 
-            using (var sr = new StreamReader(filePath))
+            //Read input
+            using (var sr = new StreamReader(input))
             {
                 string headerLine = sr.ReadLine();
                 string line;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    var position = new Position(line);
-                    NetPosition(NetPositions, position);
+                    var p = new Position(line);
+                    //Apply logic depending of the type
+                    Calculators(type, Positions, p);
                 }
             }
 
+            //Output Result
             using (var sw = new StreamWriter("Result.csv"))
             {
+
                 sw.WriteLine("TRADER,SYMBOL,QUANTITY");
                 
-                foreach(Position p in NetPositions.Values)
-                    sw.WriteLine("{0},{1},{2}",p.Trader,p.Symbol,p.Quantity);
+                foreach(Position p in Positions.Values)
+                    if (p.Net || p.Boxed)
+                        sw.WriteLine("{0},{1},{2}",p.Trader,p.Symbol,p.Quantity);
             }
         }
 
-        public static void NetPosition(SortedList NetPositions, Position p)
+        public static void Calculators(string type, SortedList Positions, Position p)
         {
+            switch (type)
+            {
+                case "NetPosition":
+                    NetPositionsCalculator(Positions, p);
+                    break;
+                case "BoxedPosition":
+                    BoxedPositionsCalculator(Positions, p);
+                    break;
+                default:
+                    throw new System.ArgumentException("Parameter cannot be null", type);
+            }
+        }
+
+        public static void NetPositionsCalculator(SortedList NetPositions, Position p)
+        {
+            p.Net = true;
             if (NetPositions.Contains(p.Hash()))
             {
                 var item = NetPositions[p.Hash()] as Position;
@@ -49,6 +70,26 @@ namespace ItvwMillenium
             }
 
             NetPositions.Add(p.Hash(), p);
+        }
+
+        public static void BoxedPositionsCalculator(SortedList BoxedPositions, Position p)
+        {
+            if (BoxedPositions.Contains(p.Hash()))
+            {
+                var item = BoxedPositions[p.Hash()] as Position;
+                if (!item.Boxed)
+                {
+                    if ((item.Quantity>0 && p.Quantity<0) || (item.Quantity<0 && p.Quantity>0))
+                    {
+                        item.Boxed = true;
+                    } 
+                }
+                item.Quantity += p.Quantity;
+                return;
+            }
+
+            BoxedPositions.Add(p.Hash(), p);
+            
         }
     }
 
@@ -59,6 +100,8 @@ namespace ItvwMillenium
         public string Symbol;
         public int Quantity;
         public int Price;
+        public bool Boxed;
+        public bool Net;
 
         public Position() {}
 
@@ -70,6 +113,8 @@ namespace ItvwMillenium
             Symbol = fields[2];
             Quantity = int.Parse(fields[3]);
             Price = int.Parse(fields[4]);
+            Net = false;
+            Boxed = false;
         }
 
         public override string ToString()
@@ -87,14 +132,21 @@ namespace ItvwMillenium
     public class UnitTest
     {
 
+        
         [TestMethod]
-        public void TestNetPositionsCalculator()
+        public void TestCalculators()
+        {
+            TestCalculator("NetPosition", "net_positions_expected.csv");
+            TestCalculator("BoxedPosition", "boxed_positions_expected.csv");
+        }
+
+
+        public void TestCalculator(string type, string expectedOutput)
         {
             var input = @"test_data.csv";
             var output = @"result.csv";
-            var expectedOutput = @"net_positions_expected.csv";
-            
-            Program.NetPositionsCalculator(input);
+
+            Program.PositionsCalculator(input, type);
             
             using (var o = new StreamReader(output))
             using (var eo = new StreamReader(expectedOutput))
@@ -110,33 +162,78 @@ namespace ItvwMillenium
         }
 
         [TestMethod]
-        public void TestNetPosition()
+        public void TestNetPositionsCalculator()
         {
             var NetPositions = new SortedList();
 
             var p1 = new Position("BuzzAldrin,DB,MOON.N,100,69");
-            Program.NetPosition(NetPositions, p1);
+            Program.NetPositionsCalculator(NetPositions, p1);
             Assert.AreEqual(1, NetPositions.Count);
             BrowseAndTestList(NetPositions, "BuzzAldrin", "MOON.N", 100);
 
             var p2 = new Position("BuzzAldrin,DB,MOON.N,100,69");
-            Program.NetPosition(NetPositions, p2);
+            Program.NetPositionsCalculator(NetPositions, p2);
             Assert.AreEqual(1, NetPositions.Count);
             BrowseAndTestList(NetPositions, "BuzzAldrin", "MOON.N", 200);
 
 
             var p3 = new Position("Steevy,DB,AAPL.N,666,85");
-            Program.NetPosition(NetPositions, p3);
+            Program.NetPositionsCalculator(NetPositions, p3);
             Assert.AreEqual(2, NetPositions.Count);
             BrowseAndTestList(NetPositions, "Steevy", "AAPL.N", 666);
 
         }
 
-        void BrowseAndTestList(SortedList NetPositions, string TargetTrader, string TargetSymbol, int TargetQuantity)
+        [TestMethod]
+        public void TestBoxedPositionsCalculator()
         {
-            var hash = string.Format("{0}{1}", TargetTrader, TargetSymbol);
-            var p = NetPositions[hash] as Position;
-            Assert.AreEqual(TargetQuantity, p.Quantity);
+            var BoxedPositions = new SortedList();
+
+            var p1 = new Position("AmazingRandy,DB,ESC.N,100,69");
+            Program.BoxedPositionsCalculator(BoxedPositions, p1);
+            Assert.AreEqual(1, BoxedPositions.Count);
+            BrowseAndTestList(BoxedPositions, "AmazingRandy", "ESC.N", 100);
+            TestBoxedPosition(BoxedPositions, "AmazingRandy", "ESC.N", false);
+
+            var p2 = new Position("AmazingRandy,DB,ESC.N,-50,69");
+            Program.BoxedPositionsCalculator(BoxedPositions, p2);
+            Assert.AreEqual(1, BoxedPositions.Count);
+            BrowseAndTestList(BoxedPositions, "AmazingRandy", "ESC.N", 50);
+            TestBoxedPosition(BoxedPositions, "AmazingRandy", "ESC.N", true);
+
+
+            var p3 = new Position("AmazingRandy,DB,HOUD.N,-300,98");
+            Program.BoxedPositionsCalculator(BoxedPositions, p3);
+            Assert.AreEqual(2, BoxedPositions.Count);
+            BrowseAndTestList(BoxedPositions, "AmazingRandy", "HOUD.N", -300);
+            TestBoxedPosition(BoxedPositions, "AmazingRandy", "HOUD.N", false);
+
+            var p4 = new Position("AmazingRandy,DB,HOUD.N,-300,98");
+            Program.BoxedPositionsCalculator(BoxedPositions, p4);
+            Assert.AreEqual(2, BoxedPositions.Count);
+            BrowseAndTestList(BoxedPositions, "AmazingRandy", "HOUD.N", -600);
+            TestBoxedPosition(BoxedPositions, "AmazingRandy", "HOUD.N", false);
+
+            var p5 = new Position("AmazingRandy,DB,HOUD.N,800,98");
+            Program.BoxedPositionsCalculator(BoxedPositions, p5);
+            Assert.AreEqual(2, BoxedPositions.Count);
+            BrowseAndTestList(BoxedPositions, "AmazingRandy", "HOUD.N", 200);
+            TestBoxedPosition(BoxedPositions, "AmazingRandy", "HOUD.N", true);
+
+        }
+
+        void BrowseAndTestList(SortedList positions, string targetTrader, string targetSymbol, int targetQuantity)
+        {
+            var hash = string.Format("{0}{1}", targetTrader, targetSymbol);
+            var p = positions[hash] as Position;
+            Assert.AreEqual(targetQuantity, p.Quantity);
+        }
+
+        void TestBoxedPosition(SortedList positions, string targetTrader, string targetSymbol, bool targetBoxed)
+        {
+            var hash = string.Format("{0}{1}", targetTrader, targetSymbol);
+            var p = positions[hash] as Position;
+            Assert.AreEqual(targetBoxed, p.Boxed);
         }
         
     }
